4c4c6b0b20d1fd08360e17ad48d3375a27773f9f
Sai Likhith Narendula <narendulasailikhith@gmail.com>
2023-08-20 18:58:26 +0530

    Front-end updated

---
+++ front-end/FinanceApp/scripts/loanapplicationsbyfilter.js
@@ -0,0 +1,88 @@
+function getLoanApplications(){
+    
+    const selectObject = document.getElementById('filterSelect')
+    //'options' returns an array of all options present in the SELECT element
+    const allOptions = selectObject.options
+    //get the index of the selected option
+    const index = selectObject.selectedIndex
+    //now get the selected option object from the SELECT element
+    const selectedOption = allOptions[index]
+    //console.log(selectedOption.value);
+    
+
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = function () {
+        if (req.status === 200 && req.readyState === 4) {
+            //console.log(req.responseText)
+            const jsData = JSON.parse(req.responseText)
+            const tableBody = document.getElementById('tblBody')
+            tableBody.innerHTML = ""
+            //console.log(jsData)
+
+
+            for (const data of jsData.responseData) {
+
+
+                    const tdId = document.createElement('td')
+                    tdId.innerText = data.applicationId.toString()
+
+                    const tdcusId = document.createElement('td')
+                    tdcusId.innerText = data.customerId.toString()
+
+                    const tdloanId = document.createElement('td')
+                    tdloanId.innerText = data.loanId.toString()
+                    
+                    const tdloanAmount = document.createElement('td')
+                    tdloanAmount.innerText = data.loanAmount.toString()
+
+                    const tdinterestRate = document.createElement('td')
+                    tdinterestRate.innerText = data.interestRate.toString()
+
+                    const tdtenure = document.createElement('td')
+                    tdtenure.innerText = data.tenure.toString()
+
+                    const tdapplicationDate = document.createElement('td')
+                    tdapplicationDate.innerText = data.applicationDate.toString().slice(0,-1)
+                    //tdapplicationDate.innerText = data.applicationDate.toString()
+
+                    const tdapplicationStatus = document.createElement('td')
+                    tdapplicationStatus.innerText = data.applicationStatus.toString()
+
+                    const tdremarks= document.createElement('td')
+                    tdremarks.innerText = (data.remarks==undefined)?"-":data.remarks.toString()
+
+                    // const tdcusId = document.createElement('td')
+                    // tdcusId.innerText = data.applicationDate.toString()
+
+                    const imgEle = document.createElement('img')
+                    imgEle.src = data.documentOne
+                    imgEle.alt = "NA"
+                    imgEle.style.width = "200px";
+                    imgEle.style.margin = "2px";
+
+                    const tdImg = document.createElement('td')
+                    tdImg.appendChild(imgEle)
+
+                    const row = document.createElement('tr')
+                    row.append(tdId,tdcusId,tdloanId,tdloanAmount,tdinterestRate,tdtenure,tdapplicationDate,tdapplicationStatus,tdremarks,tdImg)
+
+                    tableBody.append(row)
+                }
+        }
+        
+    }
+    req.open('GET', `http://localhost:8080/FinanceProjectAppDay2/rest/loan/applications/get/type?type=${selectedOption.value}`)
+    const token=localStorage.getItem('token')
+    req.setRequestHeader('Authorization', `Bearer ${token}`)
+    req.send() 
+}
+
+
+window
+    .addEventListener(
+        'DOMContentLoaded',
+        function () {
+            getLoanApplications();
+            this.document.getElementById('filterSelect').addEventListener('change',getLoanApplications);
+        }
+    )
---
+++ front-end/FinanceApp/scripts/pendingapplications.js
@@ -0,0 +1,119 @@
+function getLoanApplications(){
+    
+    const req = new XMLHttpRequest()
+    req.onreadystatechange = function () {
+        if (req.status === 200 && req.readyState === 4) {
+            //console.log(req.responseText)
+            const jsData = JSON.parse(req.responseText)
+            const tableBody = document.getElementById('tblBody')
+            tableBody.innerHTML = ""
+            //console.log(jsData)
+
+
+            for (const data of jsData.responseData) {
+
+                    const tdId = document.createElement('td')
+                    tdId.innerText = data.applicationId.toString()
+
+                    const tdcusId = document.createElement('td')
+                    tdcusId.innerText = data.customerId.toString()
+
+                    const tdloanId = document.createElement('td')
+                    tdloanId.innerText = data.loanId.toString()
+                    
+                    const tdloanAmount = document.createElement('td')
+                    tdloanAmount.innerText = data.loanAmount.toString()
+
+                    const tdinterestRate = document.createElement('td')
+                    tdinterestRate.innerText = data.interestRate.toString()
+
+                    const tdtenure = document.createElement('td')
+                    tdtenure.innerText = data.tenure.toString()
+
+                    const tdapplicationDate = document.createElement('td')
+                    tdapplicationDate.innerText = data.applicationDate.toString().slice(0,-1)
+
+                    const tdStatusElement = document.createElement("td");
+                    // Create the "Approve" button
+                    const approveButton = document.createElement("button");
+                    approveButton.innerText = "Approve";
+                    approveButton.classList.add('btn','btn-success')
+                    approveButton.addEventListener("click", () => {
+                        
+                        const req=new XMLHttpRequest();
+                        req.onreadystatechange = () =>{
+                        if (req.status === 200 && req.readyState === 4) {
+                            console.log("Yes");
+                            
+                            window.alert("Loan Application Approved!");
+                            location.reload();                            
+                        }
+                    }
+                        
+                        req.open('POST', `http://localhost:8080/FinanceProjectAppDay2/rest/loan/applications/updatestatus/${tdId.innerText}?status=approved`);
+                        req.setRequestHeader("Content-Type","application/json")
+                        req.send()
+                    
+                    });
+
+                    // Create the "Reject" button
+                    const rejectButton = document.createElement("button");
+                    rejectButton.innerText = "Reject";
+                    rejectButton.classList.add('btn','btn-danger')
+                    rejectButton.addEventListener("click", () => {
+                        const req=new XMLHttpRequest();
+                        req.onreadystatechange = ()=>{
+                        if (req.status === 200 && req.readyState === 4) {
+                            window.alert("Loan Application Rejected!");
+                            location.reload();                            
+                        }
+                    }
+                        req.open('POST', `http://localhost:8080/FinanceProjectAppDay2/rest/loan/applications/updatestatus/${tdId.innerText}?status=rejected`);
+                        req.send()
+                    });
+
+                    // Append the buttons to the td element
+                    tdStatusElement.appendChild(approveButton);
+                    tdStatusElement.appendChild(rejectButton);
+
+                    //tdapplicationDate.innerText = data.applicationDate.toString()
+
+                    // const tdapplicationStatus = document.createElement('td')
+                    // tdapplicationStatus.innerText = data.applicationStatus.toString()
+
+                    // const tdremarks= document.createElement('td')
+                    // tdremarks.innerText = (data.remarks==undefined)?"-":data.remarks.toString()
+
+                    // const tdcusId = document.createElement('td')
+                    // tdcusId.innerText = data.applicationDate.toString()
+
+                    const imgEle = document.createElement('img')
+                    imgEle.src = data.documentOne
+                    imgEle.alt = "NA"
+                    imgEle.style.width = "200px";
+                    imgEle.style.margin = "2px";
+
+                    const tdImg = document.createElement('td')
+                    tdImg.appendChild(imgEle)
+
+                    const row = document.createElement('tr')
+                    row.append(tdId,tdcusId,tdloanId,tdloanAmount,tdinterestRate,tdtenure,tdapplicationDate,tdImg,tdStatusElement)
+
+                    tableBody.append(row)
+                }
+        }
+    }
+    req.open('GET', 'http://localhost:8080/FinanceProjectAppDay2/rest/loan/applications/get/pending')
+    const token=localStorage.getItem('token')
+    req.setRequestHeader('Authorization', `Bearer ${token}`)
+    req.send()
+}
+
+
+window
+    .addEventListener(
+        'DOMContentLoaded',
+        function () {
+            getLoanApplications();
+        }
+    )
